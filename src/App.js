1ec3292256ee07cb10ac1a25af3b3ee3bbbc2b97
Tam Love <dark_angel4eva@email.com>
2021-04-11 14:08:55 +0100

    Seperate search UI.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
+import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -13,30 +14,17 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  updateScreen = (show) => {
+    this.setState(() => ({
+      showSearchPage: show
+    }))
+  }
+
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooks onChangeScreen={this.updateScreen}/>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
